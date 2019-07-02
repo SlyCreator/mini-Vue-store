4f805aef9b83735003fe50263012e5c80cea79bd
slycreator <chinonso1amaechi@gmial.com>
2019-07-02 02:19:07 +0100

    first commit

---
+++ src/pages/Cart.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>
+        <div>
+            <h1>{{ms}}</h1>
+        </div>
+    </div>
+</template>
+<script>
+export default {
+    name: 'home',
+    data() {
+        return {
+            msg: 'Welcome to the Cart page'
+        }
+    },
+}
+</script>
